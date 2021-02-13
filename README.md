73cf1844c276f61c5d73464da1b93ddbc83176d4
Alex W <alexfromapex@gmail.com>
2021-02-13 17:49:57 -0500

    - Added last modified info

--- README.md
+++ README.md
@@ -1,9 +1,11 @@
 # Filez
 
+![Filez Logo](https://raw.githubusercontent.com/alexfromapex/filez/master/static/favicon/android-chrome-192x192.png)
+
 ## Get started
 
 - Install rust tools (see [https://www.rust-lang.org/tools/install](https://www.rust-lang.org/tools/install))
-- Run git clone `https://github.com/alexfromapex/filez.git`
+- Run `git clone https://github.com/alexfromapex/filez.git`
 - Change into downloaded git repo directory: `cd filez`
 - Run `cargo build`
 - Finally `cargo run` should launch server locally
@@ -17,8 +19,6 @@
 
 ## License info
 - [License](LICENSE.md)
-
-![Filez Logo](https://raw.githubusercontent.com/alexfromapex/filez/master/static/favicon/android-chrome-192x192.png)
 
 
 ![Filez Screenshot](https://user-images.githubusercontent.com/1907805/107860652-66e7d900-6e0e-11eb-814d-eb8dd2162d95.png)
--- src/filez.rs
+++ src/filez.rs
@@ -1,17 +1,22 @@
 extern crate askama;
+extern crate chrono;
 
 use std::fs::{self};
 use std::path::{PathBuf};
 use askama::Template;
 use rocket::response::content::Html;
 use rocket::response::{NamedFile};
+use rocket::response::status::{NotFound};
 use rocket::http::RawStr;
+use chrono::offset::Utc;
+use chrono::DateTime;
 
 struct File {
     name: String,
     shortname: String,
-    size: u64,
-    isdir: bool
+    size: String,
+    modified: String,
+    // isdir: bool
 }
 
 #[derive(Template)]
@@ -23,6 +28,23 @@ struct FileListTemplate<'a> {
     filez: &'a Vec<File>
 }
 
+fn bytes_to_readable(size: u64) -> String {
+    let sz_float = size as f64;
+    let kilobyte: f64 = 1000.0;
+    let megabyte: f64 = kilobyte * 1000.0;
+    let gigabyte: f64 = megabyte * 1000.0;
+    let terabyte: f64 = gigabyte * 1000.0;
+    let petabyte: f64 = terabyte * 1000.0;
+    match sz_float {
+        sz if sz < kilobyte => format!("{} bytes", sz),
+        sz if sz < megabyte => format!("{:.2} kb", sz / kilobyte),
+        sz if sz < gigabyte => format!("{:.2} mb", sz / megabyte),
+        sz if sz < terabyte => format!("{:.2} gb", sz / gigabyte),
+        sz if sz < petabyte => format!("{:.2} tb", sz / terabyte),
+        _ => format!("gigantic")
+    }
+}
+
 fn show_filez(location: String) -> NamedFile {
     let file = PathBuf::from(&location);
     NamedFile::open(&file).unwrap()
@@ -32,24 +54,31 @@ fn list_filez(location: String) -> Html<String> {
     let base_dir = PathBuf::from(".");
     let relative = &base_dir.join(&location);
     let paths = fs::read_dir(&relative).unwrap();
-    let dir = fs::canonicalize(&relative).unwrap().display().to_string();
+    let canonical_dir = fs::canonicalize(&relative).unwrap().display().to_string();
     let mut file_list: Vec<File> = Vec::new();
     let mut _p;
+    let mut _metadata;
+    let mut _file_size;
+    let mut _datetime: DateTime<Utc>;
 
     for path in paths {
         _p = PathBuf::from(path.unwrap().path());
+        _metadata = fs::metadata(&_p).unwrap();
+        _file_size = _metadata.len();
+        _datetime = _metadata.modified().unwrap().into();
         file_list.push(File {
             name: PathBuf::from(&_p).display().to_string(),
-            shortname: String::from(PathBuf::from(&_p).strip_prefix(&dir).unwrap().to_str().unwrap()),
-            size: fs::metadata(&_p).unwrap().len(),
-            isdir: PathBuf::from(&_p).is_dir()
+            shortname: String::from(PathBuf::from(&_p).strip_prefix(&canonical_dir).unwrap().to_str().unwrap()),
+            size: bytes_to_readable(_file_size),
+            modified: format!("{} UTC", _datetime.format("%m/%d/%Y %T")),
+            // isdir: PathBuf::from(&_p).is_dir()
         })
     }
 
     let t = FileListTemplate{
         title: "Filez",
         language: "en",
-        current_directory: &dir,
+        current_directory: &canonical_dir,
         filez: &file_list
     };
     let rendered_page = t.render();
@@ -61,30 +90,34 @@ fn list_filez(location: String) -> Html<String> {
     }
 }
 
-// TODO: Need to accept both return types
-fn render_from_path(p: PathBuf) -> Result<Html<String>, NamedFile> {
+fn render_from_path(p: PathBuf) -> Result<Html<String>, Result<NamedFile, NotFound<&'static str>>> {
     let relative_string = fs::canonicalize(&p).expect("Cannot expand path");
     
-    match &p.is_dir() {
+    match &p.exists() & &p.is_dir() {
         true => {
             let file_list = list_filez(String::from(relative_string.to_str().unwrap()));
             Ok(file_list)
         },
         false => {
-            let file_view = show_filez(String::from(relative_string.to_str().unwrap()));
-            Err(file_view)
+            match &p.exists() {
+                true => {
+                    let file_view = show_filez(String::from(relative_string.to_str().unwrap()));
+                    Err(Ok(file_view))
+                },
+                false => Err(Err(NotFound("File or directory not found")))
+            }
         }
     }
 }
 
 #[get("/")]
-pub fn index() -> Result<Html<String>, NamedFile> {
+pub fn index() -> Result<Html<String>, Result<NamedFile, NotFound<&'static str>>> {
     let root_dir = PathBuf::from("./");
     render_from_path(root_dir)
 }
 
 #[get("/<relative_path>")]
-pub fn filez(relative_path: &RawStr) -> Result<Html<String>, NamedFile> {
+pub fn filez(relative_path: &RawStr) -> Result<Html<String>, Result<NamedFile, NotFound<&'static str>>> {
     let root_dir = PathBuf::from("./");
     let url = String::from(&relative_path.to_string());
     let full_path = root_dir.join(&url);
